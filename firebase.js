4d40199384bab7299bd510a6145bc8acff87d1d6
mohsinalimughal <mohsinalimughal112233445566@gmail.com>
2025-01-25 05:25:51 -0800

    ok

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
- import  { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+ import  { getAuth  , GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 
   const firebaseConfig = {
--- login.js
+++ login.js
@@ -1,6 +1,7 @@
-import {  signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { Auth } from "./firebase.js";
+import {  signInWithEmailAndPassword, signInWithPopup,GoogleAuthProvider  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { Auth} from "./firebase.js";
 
+const Provider = new GoogleAuthProvider();
 
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
@@ -26,5 +27,37 @@ form.addEventListener("submit",(event)=>{
     console.log(password.value)
 })
 
+
+const google = document.querySelector("#google")
+
+
+google.addEventListener("click",()=>{
+  console.log("google clickedk")
+
+  signInWithPopup(Auth, Provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user)
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.error("Google Sign-In error:", error);
+    console.error("Error code:", error.code);
+    console.error("Error message:", error.message);
+    // ...
+  });
+
+})
 
 
